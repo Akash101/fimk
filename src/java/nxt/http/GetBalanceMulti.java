d2dd816411af36dbb5783d98f55df08f375a60b0
akashfimk <bombaytogoa@gmail.com>
2014-09-11 11:28:46 +0100

    Changes made to remove the method getaccounts from JSONDATA and  moved to GetBalanceMulti

--- src/java/nxt/http/GetBalanceMulti.java
+++ src/java/nxt/http/GetBalanceMulti.java
@@ -4,23 +4,35 @@ import java.util.List;
 
 import nxt.Account;
 import nxt.NxtException;
+import nxt.util.Convert;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.JSONStreamAware;
 
 import javax.servlet.http.HttpServletRequest;
 
 public final class GetBalanceMulti extends APIServlet.APIRequestHandler {
 
-    static final GetBalanceMulti instance = new GetBalanceMulti();
+	static final GetBalanceMulti instance = new GetBalanceMulti();
 
-    private GetBalanceMulti() {
-        super(new APITag[] {APITag.ACCOUNTS}, "account");
-    }
+	private GetBalanceMulti() {
+		super(new APITag[] {APITag.ACCOUNTS}, "account");
+	}
 
-    @Override
-    JSONStreamAware processRequest(HttpServletRequest req) throws NxtException {
-        List<Account> accounts = ParameterParser.getAccounts(req);
-        return JSONData.accountsBalance(accounts);
-    }
+	@Override
+	JSONStreamAware processRequest(HttpServletRequest req) throws NxtException {
+		List<Account> accounts = ParameterParser.getAccounts(req);
+		JSONObject response = new JSONObject();
+		JSONArray accountsJSONArray = new JSONArray();
+		JSONObject json;
+		for (Account account : accounts ) {
+		    json = JSONData.accountBalance(account);
+			json.put("account", Convert.rsAccount(account.getId())); 
+			accountsJSONArray.add(json);
+		}
+		response.put("accounts", accountsJSONArray);
+		return response;
+	}
 
 }
